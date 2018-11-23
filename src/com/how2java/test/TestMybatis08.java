5d944ff373f8360fc5b6b4bfcfe6d7fcf2ef7d0d
unknown <hufeng0714@163.com>
2018-11-23 18:58:19 +0800

    first commit

---
+++ src/com/how2java/test/TestMybatis08.java
@@ -0,0 +1,36 @@
+package com.how2java.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.how2java.pojo.Category;
+//多条件查询
+public class TestMybatis08 {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+  
+        Map<String,Object> params = new HashMap<>();
+        params.put("id", 1);
+        params.put("name", "cat");
+         
+        List<Category> cs = session.selectList("listCategoryByIdAndName",params);
+        for (Category c : cs) {
+            System.out.println(c.getName());
+        }
+ 
+        session.commit();
+        session.close();
+  
+    }
+}
